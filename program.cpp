3321d9f5e46da48c546cfd263155287ea013f52b
Benjamin Chang <benjamin.30805@yahoo.com.tw>
2016-12-05 17:14:06 +0800

    最初的程式模型
    ω等於1~4時，將資料塊以1~4塊為一組，檢查各組有幾個錯誤的bit，十分陽春的版本。

---
+++ program.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<ctime> 
+#include<vector>
+using namespace std;
+#define sectorSize 10
+#define sectors 24
+void omega(int range);
+
+bool data[sectors][sectorSize] = {};
+int count[sectors] = {};
+
+int main(){
+	srand (time(NULL));
+	int den = 10;
+	int a;
+	for(int i = 0;i < sectors;i++){
+		for(int j = 0;j < sectorSize;j++){
+			if(rand() % den + 1 == 1){
+				data[i][j] = 1;
+				++count[i];
+			}
+		}
+	}
+
+	omega(1);
+	omega(2);
+	omega(3);
+	omega(4);
+}
+
+void omega(int range){
+	vector<int> result;
+	int temp = 0;
+	for(int i = 0;i < sectors;i++){
+		temp += count[i];
+		if((i + 1) % range == 0 || i + 1 == 24){
+			result.push_back(temp);
+			temp = 0;
+		}
+	}
+	cout << endl;
+	for(int i = 0;i < result.size();i++)
+		cout << result[i] << " ";
+	cout << endl;
+}
